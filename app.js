1595a476537ca1fdb3232a50a06094dc7c341d66
Rohan akode <rohanakode12@gmail.com>
2025-02-22 20:16:12 +0530

    Fixed error.ejs case issue

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).render("Error.ejs", { message });
+  res.status(statusCode).render("error.ejs", { message });
   // res.status(statusCode).send(message);
 });
 
